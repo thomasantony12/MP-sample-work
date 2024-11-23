274e3fe2cc43074297add642bbc8311d20210532
Thomas antony <77287334+thomasantony12@users.noreply.github.com>
2024-11-23 14:30:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ const corsOptions = {
 
 const db = new pg.Pool({
   connectionString: process.env.POSTGRES_URL,
+  user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 db.connect();
